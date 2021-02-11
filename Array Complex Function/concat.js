80d7a2a41e4e44cf8a82257930a5bc63292c074b
Sajib Hossain <43653177+Sajib-Hossain@users.noreply.github.com>
2021-02-12 02:24:38 +0600

    I will added array3

---
+++ Array Complex Function/concat.js
@@ -0,0 +1,11 @@
+/**jokhon amader ak ba akadik array[] ak korar proyojon hoi
+ *  tokhon amra concat() function use kori
+ *  concat() function muloto ak ba akadik array k aktro kore akta array banai   */
+
+var array1 = [1,2,3,4,5];
+var array2 = [6,7,8,9,10];
+var array3 = [11,12,13,14,15];
+
+var concatArray = array1.concat(array2,array3);
+
+console.log("The concat Array is: " + concatArray);
